f8d11f9a3362be62751f6966541d4654132f805f
Parot Enzo <parot23@mmi-dev.unilim.fr>
2025-04-08 16:33:16 +0200

    sauvegarde

--- app/component/Movie/script.js
+++ app/component/Movie/script.js
@@ -1,22 +1,13 @@
-let templateFile = await fetch("./component/NavBar/Movie/template.html");
+let templateFile = await fetch("./component/Movie/template.html");
 let template = await templateFile.text();
 
 let Movie = {};
-Movie.format = function (movies) {
-  // préventio s'il n'y a pas de films
-  if (movies.length === 0) {
-    return "<p>Il n'y a pas de film dans la catégorie.</p>";
-  }
-  let html = "";
-  movies.forEach((movie) => {
-    let movieHtml = template;
-    movieHtml = movieHtml.replace("{{titre}}", movie.name);
-    movieHtml = movieHtml.replace("{{image}}", movie.image);
-    movieHtml = movieHtml.replace("{{handler}}",`C.handlerDetail(${movie.id})`);
 
-    html += movieHtml;
-  });
+Movie.format = function (movies) {
+  let html = template;
+  html = html.replace("{{image}}", movie.image);
+  html = html.replace("{{name}}", movie.name);
   return html;
 };
 
-export { Movie };
+export {Movie};
